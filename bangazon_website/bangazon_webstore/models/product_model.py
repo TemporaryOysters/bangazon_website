a19e7a938e26d5dcfd6ff90ac457003ba8834a18
Joey Kirby <jkirbymusic@gmail.com>
2017-02-23 10:36:12 -0600

    Additional tests for Product

--- bangazon_website/bangazon_webstore/models/product_model.py
+++ bangazon_website/bangazon_webstore/models/product_model.py
@@ -6,6 +6,39 @@ class Product(models.Model):
     name = models.CharField(max_length=75)
     description = models.TextField()
     price = models.IntegerField()
+    quantity = models.IntegerField()
     # seller = models.ForeignKey(customer.Customer)
     # product_category = models.ForeignKey(product_category.ProductCategory)
-    # created_at = models.DateTimeField(auto_now_add=True, editable=False)
+
+    def get_product_name(self):
+        return self.name
+
+    def get_description(self):
+        return self.description
+
+    def get_price(self):
+        return self.price
+
+    def get_quantity(self):
+        return self.quantity
+
+    def purchase_product(self, itemQty):
+        if itemQty > self.quantity:
+            print("We don't have that kind of quantity for {}. Only {} available".format(self.name, self.quantity))
+        else:
+            self.quantity = self.quantity - itemQty
+            return self.quantity
+
+    def seller_set_quantity(self, new_quantity):
+        self.quantity = new_quantity
+        return self.quantity
+
+    def get_purchased(self):
+        return self.purchased
+
+    def get_seller(self):
+        pass
+
+    def get_product_category(self):
+        pass
+
--- bangazon_website/bangazon_webstore/tests/TestProduct.py
+++ bangazon_website/bangazon_webstore/tests/TestProduct.py
@@ -17,21 +17,30 @@ class TestProduct(TestCase):
         
     Author: Joey Kirby, TempOysters
     '''
+    print("3 tests are coming from TestProduct")
 
     @classmethod
     def setUp(self):
-        self.kickball = product_model.Product(name="kickball", description="Round Ball", price=5)
+        self.kickball = product_model.Product(name="kickball", description="Round Ball", price=5, quantity=3)
         # seller="Joey", product_category="Toys"
         self.kickball.save()
 
     def test_product_model_can_be_created_with_correct_fields(self):
         self.assertIsInstance(self.kickball, product_model.Product)
 
-        self.assertEqual(self.kickball.name, "kickball")
-        self.assertEqual(self.kickball.description, "Round Ball")
-        self.assertEqual(self.kickball.price, 5)
+        self.assertEqual(self.kickball.get_product_name(), "kickball")
+        self.assertEqual(self.kickball.get_description(), "Round Ball")
+        self.assertEqual(self.kickball.get_price(), 5)
+        self.assertEqual(self.kickball.get_quantity(), 3)
         # self.assertEqual(kickball.seller, "Joey")
         # self.assertEqual(kickball.name, "Toys")
+
+    def test_purchase_product(self):
+        self.kickball.purchase_product(1)
+        self.assertEqual(self.kickball.get_quantity(), 2)
+
+    def test_seller_can_set_new_quantity(self):
+        self.assertEqual(self.kickball.seller_set_quantity(50), 50)
 
 if __name__ == '__main__':
     main()
