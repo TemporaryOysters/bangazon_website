d18b47c5887be2513eee1724958cb16df1b44e01
Zachary Cline <zcline08@gmail.com>
2017-02-23 11:06:38 -0600

    empty

--- bangazon_website/bangazon_webstore/models/paymenttypes.py
+++ bangazon_website/bangazon_webstore/models/paymenttypes.py
@@ -4,7 +4,7 @@ from django.db import models
 class PaymentType(models.Model):
     
     """PaymentType model class
-        The purpose of this class is to define customer's payment types
+        The purpose of this class is to define a customer's payment types
         author: Zachary
         methods: string return
         meta: plural name
@@ -14,7 +14,7 @@ class PaymentType(models.Model):
     cvv = models.CharField(max_length=4)
     expiration = models.DateField(auto_now=False)
     name_on_card = models.CharField(max_length=55)
-    customer = models.ForeignKey(Customer, null=True)
+    # customer = models.ForeignKey(Customer, null=True)
 
     class Meta:
         verbose_name_plural = "PaymentTypes"
@@ -23,23 +23,23 @@ class PaymentType(models.Model):
         return '{} {} {} {} {} {}'.format(self.card_type, self.card_number, self.cvv, self.expiration, self.name_on_card, self.customer,)
 
     def get_name_on_card(self):
-        return self.__name_on_card
+        return self.name_on_card
 
     def get_card_type(self):
-        return self.__card_type
+        return self.card_type
 
     def get_card_number(self):
-        return self.__card_number
+        return self.card_number
 
     def get_expiration(self):
-        return self.__expiration
+        return self.expiration
 
     def get_cvv(self):
-        return self.__cvv
+        return self.cvv
 
     def get_customer(self):
-        return self.__customer
+        return self.customer
 
 
     def get_customer_name(self):
-        return self.__customer.get_full_name()
+        return self.customer.get_full_name()
--- bangazon_website/bangazon_webstore/tests/testPaymentTypes.py
+++ bangazon_website/bangazon_webstore/tests/testPaymentTypes.py
@@ -19,20 +19,20 @@ class TestPaymentType(TestCase):
     """
 
     @classmethod
-    def setUpClass(self):
+    def setUp(self):
 
 
-        self.zach = Customer(
-        id= 1,
-        first_name = "Zachary", 
-        last_name="Cline", 
-        email = "z@s.com",
-        phone_number="555-999-4444",
-        city = "Nah Penzance" ,
-        state = "Rhode Island" ,
-        postalZip = "52801",
-        address = "300 Winter's End"
-        )
+        # self.zach = Customer(
+        # id= 1,
+        # first_name = "Zachary", 
+        # last_name="Cline", 
+        # email = "z@s.com",
+        # phone_number="555-999-4444",
+        # city = "Nah Penzance" ,
+        # state = "Rhode Island" ,
+        # postalZip = "52801",
+        # address = "300 Winter's End"
+        # )
 
         self.visa = PaymentType(
         card_type = "Visa",
@@ -40,12 +40,22 @@ class TestPaymentType(TestCase):
         cvv = 111,
         expiration = "08/20",
         name_on_card = "Zachary A Cline",
-        customer= self.zach.id)
+        # customer= self.zach.id
+        )
 
 
     def test_can_create_a_payment_type(self):
         # Test is visa a type of PaymentType
         self.assertIsInstance(self.visa, PaymentType)
+
+    def test_payment_has_properties(self):
+        # Testing if visa Has All Required Field Properties
+        self.assertIsNotNone(self.visa.get_name_on_card())
+        self.assertIsNotNone(self.visa.get_card_type())
+        self.assertIsNotNone(self.visa.get_card_number())
+        self.assertIsNotNone(self.visa.get_expiration())
+        self.assertIsNotNone(self.visa.get_cvv())
+        # self.assertIsNotNone(self.visa.get_customer())
 
     def test_if_payment_has_relevant_properties(self):
         self.assertEqual("Zachary A Cline", self.visa.get_name_on_card())
@@ -53,7 +63,7 @@ class TestPaymentType(TestCase):
         self.assertEqual("2224-333-3344", self.visa.get_card_number())
         self.assertEqual("08/20", self.visa.get_expiration())
         self.assertEqual(111, self.visa.get_cvv())
-        self.assertEqual(self.zach.id, self.visa.get_customer())
+        # self.assertEqual(self.zach.id, self.visa.get_customer())
 
 
 
