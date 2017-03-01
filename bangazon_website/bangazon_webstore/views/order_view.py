ec6705ce9c531c5753b484405acfa7697ee49a68
Mark Ellis <markellisdev@gmail.com>
2017-03-01 10:40:57 -0600

    order view and template continued

--- bangazon_website/bangazon_webstore/views/order_view.py
+++ bangazon_website/bangazon_webstore/views/order_view.py
@@ -15,17 +15,8 @@ def get_bangazon_orders(request):
     Returns all Bangazon Orders in a list form
     Author: Mark Ellis
     """
-    with sqlite3.connect('../db.sqlite3') as conn:
-        c = conn.cursor()
-        command = """
-        SELECT order
-        FROM BangazonOrder
-        """
-        c.execute(command)
-        all_orders = c.fetchall()
-        return all_orders
+    if request.user.is_authenticated() && user.id == customer.user
+        authenticated_customer = customer.id
+    all_orders_queryset = bangazon_order_model.BangazonOrder.objects.filter(authenticated_customer)
 
-
-
-    # all_orders = bangazon_order_model.BangazonOrder.objects.filter()
-
+# Create product order join table
