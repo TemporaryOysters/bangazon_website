aeced89252919ae94774d78e3a274fa062a2b448
Mark Ellis <markellisdev@gmail.com>
2017-03-09 06:06:04 -0600

    cart working without payment types

--- bangazon_website/bangazon_webstore/urls.py
+++ bangazon_website/bangazon_webstore/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url, include
 import sys
 # sys.path.append("../")
 from django.contrib import admin
-from .views import customer_view, product_detail_view, product_view, home_view, product_category_view
+from .views import customer_view, product_detail_view, product_view, home_view, product_category_view, bangazon_order_view
 from .views.customer_view import RegisterViewSet, LoginViewSet
 from .views.payment_type_view import add_payment
 from .views.create_product_view import create_a_product
@@ -18,7 +18,7 @@ urlpatterns = [
     url(r'^logout/', customer_view.logout_customer, name='logout'),
     url(r'^order/', customer_view.logout_customer, name='order'),
     url(r'^account/', customer_view.logout_customer, name='account'),
-    url(r'^cart/', customer_view.logout_customer, name='cart'),
+    url(r'^cart/', bangazon_order_view.get_products_in_cart, name='cart'),
     url(r'^addpayment/', add_payment, name='addpayment'),
 ]
 
